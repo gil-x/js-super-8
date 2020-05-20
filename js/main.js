6d3599fe6679f87961e782e98aa2d0969e96b9a0
gil <gino.ladowitch@gmail.com>
2020-05-20 20:03:09 +0200

    DOM & images preload OK.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,116 @@
-console.log('JavaScript is OK');
+class Player {
+
+    constructor (
+
+        parent=document.body,
+        videoW, videoH, FPS=24,
+        mediaFolder='media', totalImgs, totalRolls=1, imgsByRoll,
+        loop=true, loopR=false,
+
+    ) {
+       
+        // Parameters
+        this.videoW = videoW;
+        this.videoH = videoH;
+        this.FPS = FPS;
+        this.mediaFolder = mediaFolder;
+        this.totalImgs = totalImgs;
+        this.totalRolls = totalRolls;
+        this.imgsByRoll = imgsByRoll;
+        this.loop = loop;
+        this.loopR = loopR;
+
+        // Internal
+        this.intervalID;
+        this.direction = 1;
+        this.currentFrame = 1;
+        this.currentRoll = 0;
+        this.rolls = this.listRolls();
+        this.burn = false;
+        this.imagesFiles = [];
+
+        // DOM
+        this.player = this.buildDOM();
+        parent.appendChild(this.player);
+
+    }
+
+    /**
+     * Generate list of file names.
+     * @returns list
+     */
+    listRolls() {
+        let rolls = [];
+        for (let i = 0; i < this.totalRolls; i++) {
+            rolls.push(`strip_0${i < 10 ? 0 : ''}${i + 1}.png`);
+        }
+        console.log(`rolls: ${rolls}`);
+        return rolls;
+    }
+
+    preloadImages() {
+
+        let imagesURL = [];
+
+        function loadImage(url) {
+            console.log(`Loading ${url}`);
+            return new Promise((resolve, reject) => {
+                let img = new Image();
+                img.onload = () => resolve(img);
+                img.onerror = reject;
+                img.src = url;
+            });
+        }
+
+        for (let index = 0; index < this.rolls.length; index++) {
+            imagesURL[index] = `${this.mediaFolder}/${this.rolls[index]}`;
+            console.log(`imagesURL: ${imagesURL}`);
+        }
+
+        const sayhello = (images) => {
+            this.imagesFiles = [...images];
+            console.log(`hello!\n${this.imagesFiles}`);
+            this.play();
+        }
+
+        async function loadAllImages() {
+
+            let images = [];
+
+            for (let index = 0; index < imagesURL.length; index++) {
+                images[index] = await loadImage(imagesURL[index]);
+                // let imgX = await loadImage('https://picsum.photos/1920/1080');
+                // let imgY = await loadImage('https://picsum.photos/1920/2160');
+                // let imgZ = await loadImage('https://picsum.photos/2160/1080');
+            }
+            console.log(`async done`);
+            return sayhello(images);
+        }
+
+        loadAllImages();
+    }
+
+    buildDOM() {
+        this.player = document.createElement('div');
+        this.player.id = "player";
+        console.log('DOM built');
+        return this.player;
+    }
+
+    play() {
+        console.log(`Now playing!`);
+        this.player.style.background = `url(${this.imagesFiles[0].src})`;
+    }
+ 
+}
+
+const myPlayer = new Player(
+    parent=document.getElementById('container'),
+    videoW=720, videoH=480, FPS=24,
+    mediaFolder='media/stripX4',
+    totalImgs=96,
+    totalRolls=4, imgsByRoll=24,
+    loop=true, loopR=false,
+);
+
+myPlayer.preloadImages();
